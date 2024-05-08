236ea0dd8c96957637cbd8e88c1f2a07e1fe1ec1
KayanDenizo <kayan.denizo@gmail.com>
2024-05-08 20:34:20 -0300

    Files of 08-05-2024

---
+++ 06-05-2024/exs/soma.py
@@ -0,0 +1,7 @@
+x = float(input('Digite o primeiro número: '));
+y = float(input('Digite o segundo número: '));
+
+soma = x + y 
+print ('A soma é: ', soma)
+
+# CERTO
---
+++ 06-05-2024/variaveis.py
@@ -0,0 +1,18 @@
+# Faça uma funcao para somar x e y, onde x = 12, e y = 10:
+
+# CODIGO KAYLAN
+x = 12;
+y = 10; 
+
+def Somar():
+    somar = x + y;
+    print(somar);
+
+# CODIGO KAYAN
+
+x = 12;
+y = 10;
+
+def soma():
+    print(x + y);
+print(soma());
